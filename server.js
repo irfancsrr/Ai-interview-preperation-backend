2edc0f332e0082fbe7135ead1faf17bd82c14c70
irfan <irfancsrrsimt@gmail.com>
2026-04-26 20:18:18 +0530

    fix cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: [process.env.CLIENT_URL || 'http://localhost:5173','http://10.93.78.66:5173'] ,credentials:true}));
+app.use(cors({ origin: [process.env.CLIENT_URL],methods: ["GET", "POST", "PUT", "DELETE"] ,credentials:true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
